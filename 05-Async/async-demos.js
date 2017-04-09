489ff9c3d889955d245dbb501f828f54e66093ec
Magesh <tkmagesh77@gmail.com>
2017-04-09 13:32:32 +0530

    using $http

--- 05-Async/async-demos.js
+++ 05-Async/async-demos.js
@@ -53,10 +53,25 @@ var app = (function(){
 		}
 	})();
 
+	function addAsyncPromise(x,y){
+		var promise = new Promise(function(resolveFn, rejectFn){
+			console.log('	[Service] processing ', x , ' and ', y);
+			setTimeout(function(){
+				var result = x + y;
+				console.log('	[Service] returning result');
+				resolveFn(result);
+			}, 5000);
+		});
+		return promise;
+	}
+
+
+
 	return {
 		addSyncClient : addSyncClient,
 		addAsyncClient : addAsyncClient,
-		addAsyncEvents : addAsyncEvents
+		addAsyncEvents : addAsyncEvents,
+		addAsyncPromise : addAsyncPromise
 	}
 
 })();
